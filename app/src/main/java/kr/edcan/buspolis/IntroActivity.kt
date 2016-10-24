0cb0c548dc45b2c17584833910bc67051b13934c
LNTCS <lcs7958@gmail.com>
2016-10-24 15:05:16 +0900

    버튼 배경 이미지 생성 인트로페이지 작업중

--- app/src/main/java/kr/edcan/buspolis/IntroActivity.kt
+++ app/src/main/java/kr/edcan/buspolis/IntroActivity.kt
@@ -2,11 +2,18 @@ package kr.edcan.buspolis
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        Glide.with(this).load(R.drawable.bg_start).into(introBg)
+        
     }
 }
