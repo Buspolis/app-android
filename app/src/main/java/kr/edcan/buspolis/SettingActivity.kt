39c0aa3b6879cb9c455e4251dc29c0698aa84321
Junseok Oh <kotohana5706@edcan.kr>
2016-10-26 05:18:23 +0900

    세팅액티비티 리스트 레이아웃 수정: content가 없을경우 GONE처리

--- app/src/main/java/kr/edcan/buspolis/SettingActivity.kt
+++ app/src/main/java/kr/edcan/buspolis/SettingActivity.kt
@@ -10,6 +10,7 @@ import kr.edcan.buspolis.model.ListContent
 import com.github.nitrico.lastadapter.LastAdapter
 import kotlinx.android.synthetic.main.activity_setting.*
 import kr.edcan.buspolis.databinding.ContentSettingsHeaderBinding
+import kr.edcan.buspolis.databinding.ItemSettingsBinding
 import org.jetbrains.anko.toast
 import java.util.*
 
@@ -36,6 +37,7 @@ class SettingActivity : AppCompatActivity() {
         arrayList.add(ListContent("Auto Refresh", "Set Auto Refresh Time", "30s"))
         arrayList.add(ListContent("Auto Refresh", "Set Auto Refresh Time", "30s"))
         arrayList.add(ListContent("Auto Refresh", "Set Auto Refresh Time", "30s"))
+        arrayList.add(ListContent("Auto Refresh", "", "30s"))
         LastAdapter.with(arrayList, BR.item)
                 .onBindListener(object: LastAdapter.OnBindListener{
                     override fun onBind(item: Any, view: View, type: Int, position: Int) {
@@ -43,6 +45,11 @@ class SettingActivity : AppCompatActivity() {
                             R.layout.content_settings_header -> {
                                 val binding = DataBindingUtil.getBinding<ContentSettingsHeaderBinding>(view)
                                 binding.title.text = arrayList[position].toString()
+                            }
+                            R.layout.item_settings -> {
+                                val contentBinding = DataBindingUtil.getBinding<ItemSettingsBinding>(view)
+                                item as ListContent
+                                if(item.content.equals("")) contentBinding.content.visibility = View.GONE
                             }
                         }
                     }
