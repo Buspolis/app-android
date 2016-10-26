c5e48fc366646580bf0d8b7e81685bce0c3a39ce
Junseok Oh <kotohana5706@edcan.kr>
2016-10-26 14:08:14 +0900

    SearchItem type, id 추가

--- app/src/main/java/kr/edcan/buspolis/model/SearchItem.kt
+++ app/src/main/java/kr/edcan/buspolis/model/SearchItem.kt
@@ -1,14 +1,22 @@
 package kr.edcan.buspolis.model
 
+import kotlin.properties.Delegates
+
 /**
  * Created by LNTCS on 2016-10-23.
  */
 
 class SearchItem {
+    var type by Delegates.notNull<listType>()
+    var id = ""
     var keyword = ""
-    var option : Any? = null
+    var option: Any? = null
 
     constructor(keyword: String) {
         this.keyword = keyword
+    }
+
+    enum class listType {
+        BUS, BUSSTOP
     }
 }
