5c9a9aadad4c806577070d18310c9da98cc468b3
Junseok Oh <kotohana5706@edcan.kr>
2016-10-30 21:52:36 +0900

    BusFareShow, BusFareSpeak 레이아웃 완성

--- app/src/main/java/kr/edcan/buspolis/BusFareSpeakActivity.kt
+++ app/src/main/java/kr/edcan/buspolis/BusFareSpeakActivity.kt
@@ -2,11 +2,16 @@ package kr.edcan.buspolis
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_bus_fare_speak.*
+import kr.edcan.buspolis.util.KoreanRomanizer
 
 class BusFareSpeakActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bus_fare_speak)
+        helpKor.text = "${intent.getStringExtra("name")} 버스 정류장은 어디에 있나요?"
+        helpRomaji.text = KoreanRomanizer.romanize(helpKor.text.toString())
+        icBack.setOnClickListener { finish() }
     }
 }
--- app/src/main/java/kr/edcan/buspolis/util/Utils.kt
+++ app/src/main/java/kr/edcan/buspolis/util/Utils.kt
@@ -10,14 +10,14 @@ import kr.edcan.buspolis.R
  * Created by LNTCS on 2016-10-25.
  */
 
-object Utils{
+object Utils {
     fun lang(context: Context) = Prefs.with(context).read("lang")!!
 
     var ServiceKey = "rOCJOizLiZuxSqugYyn9B10Np9YWY3+ohWdwZlgNR4dD+M1kNFUxAp9Zs++RJv1eKazIVCysjbm2P3ld1VWkvQ=="
 
     var ServiceKeyS = "C49c3xuCEXZMaWm1Z7SKQQyGM6xv10cLJK+cJPaNmWleqJl8iCn/FJ/ky5z9O73A9+9Y2Y3+AYDIc0dix/MZYA=="
 
-    fun getNearStop(x: Double, y: Double, callback: AsyncHttpResponseHandler){
+    fun getNearStop(x: Double, y: Double, callback: AsyncHttpResponseHandler) {
         var params = RequestParams().apply {
             put("ServiceKey", ServiceKey)
             put("tmX", x)
@@ -29,9 +29,9 @@ object Utils{
         HttpClient.get("/stationinfo/getStationByPos", params, callback)
     }
 
-    fun convertCode(num: String) = if(num.length < 5){
+    fun convertCode(num: String) = if (num.length < 5) {
         "0${num[0]}-${num.substring(1..3)}"
-    }else{
+    } else {
         "${num.substring(0..1)}-${num.substring(2..4)}"
     }
 
@@ -45,5 +45,14 @@ object Utils{
         val titleBarArr = arrayOf(R.color.busBlueDark, R.color.busRedDark, R.color.busGreenDark, R.color.busBlueDark, R.color.busGreenDark, R.color.busYellowDark, R.color.busRedDark)
         val realPosition = if (position > titleBarArr.size) 6 else position
         return titleBarArr[realPosition]
+    }
+
+    fun busFareColor(position: Int): Int {
+        val colorArr = arrayOf(R.color.busBlue, R.color.busGreen, R.color.busYellow)
+        return colorArr[if (position > colorArr.size) 0 else position]
+    }
+    fun busFareColorDark(position: Int): Int {
+        val colorArr = arrayOf(R.color.busBlueDark, R.color.busGreenDark, R.color.busYellowDark)
+        return colorArr[if (position > colorArr.size) 0 else position]
     }
 }
