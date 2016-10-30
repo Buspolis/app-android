be897c021426c680a06971db5829d9c35c184b6c
Junseok Oh <kotohana5706@edcan.kr>
2016-10-30 18:59:05 +0900

    BusFare에 필요한 액티비티 생성

---
+++ app/src/main/java/kr/edcan/buspolis/BusFareSpeakActivity.kt
@@ -0,0 +1,12 @@
+package kr.edcan.buspolis
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BusFareSpeakActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bus_fare_speak)
+    }
+}
